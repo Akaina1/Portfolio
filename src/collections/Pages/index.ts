1c4533b85e46315b65cd96bc5adb6e7bc36b16f9
Aaron <erinadost@gmail.com>
2025-02-13 16:12:08 -0400

    added animate media block to pages collection

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -11,6 +11,7 @@ import { ProjectDisplay } from '../../blocks/ProjectDisplay/config';
 import { hero } from '@/heros/config';
 import { AnimateText } from '../../blocks/AnimateText/config';
 import { HighlightText } from '../../blocks/HighlightText/config';
+import { AnimateMedia } from '../../blocks/AnimateMedia/config';
 import { Marquee } from '../../blocks/Marquee/config';
 import { slugField } from '@/fields/slug';
 import { populatePublishedAt } from '../../hooks/populatePublishedAt';
@@ -89,6 +90,7 @@ export const Pages: CollectionConfig<'pages'> = {
                 ProjectDisplay,
                 HighlightText,
                 Marquee,
+                AnimateMedia,
               ],
               required: true,
               admin: {
