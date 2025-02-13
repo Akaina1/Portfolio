d4a5a5eefc037b34f3ae568d0e1a0bed87c664f8
Aaron <erinadost@gmail.com>
2025-02-13 16:12:41 -0400

    code formatting

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,13 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative overflow-hidden',
+  cn(
+    'inline-flex items-center justify-center whitespace-nowrap rounded text-sm',
+    'font-medium ring-offset-background transition-colors',
+    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
+    'focus-visible:ring-offset-2 disabled:pointer-events-none',
+    'disabled:opacity-50 relative overflow-hidden'
+  ),
   {
     defaultVariants: {
       size: 'default',
@@ -28,10 +34,25 @@ const buttonVariants = cva(
           'border border-border bg-background hover:bg-card hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        'nav-link':
-          'text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-all duration-200 relative font-bold active:text-pink-200/90 dark:active:text-pink-200/90 active:scale-110 dark:active:scale-110',
-        'home-hero':
-          'border border-gray-400 dark:border-gray-800 hover:scale-105 hover:shadow-lg hover:shadow-primary/20 dark:hover:shadow-primary/10 transition-all duration-700 ease-out bg-foreground/20 dark:bg-background text-black dark:text-white before:absolute before:h-64 before:w-64 before:inset-0 before:-z-10 before:translate-x-[-100%] before:translate-y-[100%] before:rotate-45 before:bg-gradient-to-r before:from-primary/40 before:via-primary/30 before:to-transparent before:transition-transform hover:before:translate-x-[-60%] hover:before:translate-y-[-50%] before:duration-1000',
+        'nav-link': cn(
+          'text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white',
+          'transition-all duration-200 relative font-bold',
+          'active:text-pink-200/90 dark:active:text-pink-200/90',
+          'active:scale-110 dark:active:scale-110'
+        ),
+        'home-hero': cn(
+          'font-mono dark:font-light border-b-2 border-r-2 border-l border-t',
+          'border-black/30 dark:border-white/15 rounded-button hover:font-bold',
+          'hover:shadow-lg hover:shadow-primary/20 dark:hover:shadow-primary/10',
+          'transition-all duration-500 ease-out bg-stone-300',
+          'dark:bg-background text-black dark:text-white relative isolate',
+          'before:absolute before:h-64 before:w-64 before:inset-0',
+          'before:translate-x-[-100%] before:translate-y-[100%] before:rotate-45',
+          'before:bg-gradient-to-r before:from-primary/30 before:via-primary/20',
+          'before:to-transparent before:transition-transform',
+          'hover:before:translate-x-[-60%] hover:before:translate-y-[-50%]',
+          'before:duration-1000'
+        ),
       },
     },
   }
