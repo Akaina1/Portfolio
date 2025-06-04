330f333a863bb47ce3baa771bdf9401b3c9b1547
Aaron <erinadost@gmail.com>
2025-06-04 06:50:48 -0300

    update robots generation

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,7 +1,7 @@
 const SITE_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ||
   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+  'https://example.com';
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
@@ -12,9 +12,14 @@ module.exports = {
     policies: [
       {
         userAgent: '*',
-        disallow: '/admin/*',
+        // TODO: Update this when going live - currently blocking all crawlers for development/staging
+        // For production, change to: disallow: '/admin/*',
+        disallow: '/',
       },
     ],
-    additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`, `${SITE_URL}/posts-sitemap.xml`],
+    additionalSitemaps: [
+      `${SITE_URL}/pages-sitemap.xml`,
+      `${SITE_URL}/posts-sitemap.xml`,
+    ],
   },
-}
+};
