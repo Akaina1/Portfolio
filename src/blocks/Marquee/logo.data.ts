c67ed3195b084658130267272cfa2a0453470243
Aaron <erinadost@gmail.com>
2025-06-04 16:08:56 -0300

    add logo.data.ts file to define DEFAULT_LOGOS for improved logo management and maintainability

---
+++ src/blocks/Marquee/logo.data.ts
@@ -0,0 +1,101 @@
+// Define a type for the logo object
+type DefaultLogo = {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+};
+
+// Default logos with specific dimensions
+export const DEFAULT_LOGOS: DefaultLogo[] = [
+  {
+    src: '/media/typescript-svgrepo-com.svg',
+    alt: 'Default Logo 1',
+    width: 90,
+    height: 90,
+  },
+  {
+    src: '/media/tailwind-svgrepo-com.svg',
+    alt: 'Default Logo 2',
+    width: 110, // Larger logo
+    height: 110,
+  },
+  {
+    src: '/media/react-svgrepo-com.svg',
+    alt: 'Default Logo 1',
+    width: 90,
+    height: 90,
+  },
+  {
+    src: '/media/nodejs-logo-svgrepo-com.svg',
+    alt: 'Default Logo 2',
+    width: 150, // Larger logo
+    height: 150,
+  },
+  {
+    src: '/media/payload.svg',
+    alt: 'Default Logo 1',
+    width: 150,
+    height: 150,
+  },
+  {
+    src: '/media/nextjs-icon-svgrepo-com.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/CSS3.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/Docker.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/Express.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/Git.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/HTML5.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/JavaScript.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/MongoDB.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/preact-svgrepo-com.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+  {
+    src: '/media/Tauri.svg',
+    alt: 'Default Logo 2',
+    width: 90, // Larger logo
+    height: 90,
+  },
+];
