522f52d72140417d5a1090cde26c5446a59eb6dc
Aaron <erinadost@gmail.com>
2025-06-04 16:08:51 -0300

    refactor Marquee component by importing DEFAULT_LOGOS from logo.data, removing inline logo definitions for improved maintainability

--- src/blocks/Marquee/Component.tsx
+++ src/blocks/Marquee/Component.tsx
@@ -3,55 +3,7 @@ import { Media } from '@/components/Media';
 import Image from 'next/image';
 import type { MarqueeBlockType } from '@/payload-types';
 import { cn } from '@/utilities/cn';
-
-// Define a type for the logo object
-type DefaultLogo = {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-};
-
-// Default logos with specific dimensions
-const DEFAULT_LOGOS: DefaultLogo[] = [
-  {
-    src: '/media/typescript-svgrepo-com.svg',
-    alt: 'Default Logo 1',
-    width: 90,
-    height: 90,
-  },
-  {
-    src: '/media/tailwind-svgrepo-com.svg',
-    alt: 'Default Logo 2',
-    width: 110, // Larger logo
-    height: 110,
-  },
-  {
-    src: '/media/react-svgrepo-com.svg',
-    alt: 'Default Logo 1',
-    width: 90,
-    height: 90,
-  },
-  {
-    src: '/media/nodejs-logo-svgrepo-com.svg',
-    alt: 'Default Logo 2',
-    width: 150, // Larger logo
-    height: 150,
-  },
-  {
-    src: '/media/payload.svg',
-    alt: 'Default Logo 1',
-    width: 150,
-    height: 150,
-  },
-  {
-    src: '/media/nextjs-icon-svgrepo-com.svg',
-    alt: 'Default Logo 2',
-    width: 90, // Larger logo
-    height: 90,
-  },
-  // Add more logos with custom dimensions
-];
+import { DEFAULT_LOGOS } from './logo.data';
 
 /**
  * PartnerReelBlock Component
